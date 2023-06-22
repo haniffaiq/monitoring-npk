9d2b034830756cf9914f96585a0e0e0522de37d7
syahdan41 <syahdantriantoro41@gmail.com>
2023-06-22 20:17:50 +0700

    apalah daya

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./style.css";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import Ilust from "../../Assets/images/ilust.png";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 function Login(props) {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [isChecked, setIsChecked] = useState(false);
   const togglePasswordVisibility = () => {
@@ -14,14 +19,26 @@ function Login(props) {
   const handleCheckboxChange = () => {
     setIsChecked(!isChecked);
   };
-  const handleClick1 = () => {
+  const handleLogin = () => {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Login berhasil, akses data pengguna melalui userCredential.user
+        const user = userCredential.user;
+        console.log("Login berhasil. ID pengguna:", user.uid);
+        navigate("/homepage");
+      })
+      .catch((error) => {
+        // Terjadi kesalahan saat login, tangani error di sini
+        console.log("Error saat login:", error.message);
+      });
     console.log("Button clicked!");
   };
   const handleClick2 = () => {
     console.log("Button clicked!");
   };
   return (
-    <div className="mainContainer">
+    <div className="mainContainerLogin">
       <img src={Ilust} className="ilust-img" />
       <div className="loginContainer">
         <div className="loginWelcomeContainer">
@@ -29,10 +46,10 @@ function Login(props) {
           <p className="loginMasukKeAkunAndaTXT">Masuk ke akun anda</p>
           <div className="login-form">
             <label htmlFor="input-username">Email</label>
-            <input type="text" id="input-username" placeholder="yourname@gmail.com" />
+            <input type="text" id="input-username" placeholder="yourname@gmail.com" value={email} onChange={(e) => setEmail(e.target.value)} />
             <label htmlFor="input-password">Password</label>
             <div className="password-input-wrapper">
-              <input type={showPassword ? "text" : "password"} id="input-password" placeholder="Minimal 8 Karakter" />
+              <input type={showPassword ? "text" : "password"} id="input-password" placeholder="Minimal 6 Karakter" value={password} onChange={(e) => setPassword(e.target.value)} />
               <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} onClick={togglePasswordVisibility} />
             </div>
             <div className="check-and-forgotpass-login">
@@ -43,7 +60,7 @@ function Login(props) {
               </label>
               <p>Lupa Password?</p>
             </div>
-            <button onClick={handleClick1} className="masuk-button-login">
+            <button onClick={handleLogin} className="masuk-button-login">
               Masuk
             </button>
             <div className="separator-container">
